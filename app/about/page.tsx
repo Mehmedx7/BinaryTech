9a036414cac4a409a25c15f01db3b63fadcd2b58
mehmedx7 <mehmedkhan16@gmail.com>
2023-11-07 19:37:51 +0530

    about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -15,7 +15,7 @@ const AboutPage = () => {
     <>
       <Breadcrumb
         pageName="About Page"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+        description="We are a team of skilled software engineers and UI designers dedicated to delivering top-notch services and solutions."
       />
       <AboutSectionOne />
       <AboutSectionTwo />
