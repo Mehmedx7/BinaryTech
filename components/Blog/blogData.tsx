f4bfefa4c7f59e1e48c9e886580c203ee201f8ac
jbtravel <it.support@jbtravels.in>
2023-11-10 11:41:30 +0530

    image

--- components/Blog/blogData.tsx
+++ components/Blog/blogData.tsx
@@ -3,9 +3,9 @@ import { Blog } from "@/types/blog";
 const blogData: Blog[] = [
   {
     id: 1,
-    title: "Best UI components for modern websites",
+    title: "EV Charging Server/UI (OCPP1.6)",
     paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+      "Full-Stack Web Application With Admin Dashboard And Paytm Payment Gateway, Ready To Use For Business.",
     image: "/images/blog/blog-01.jpg",
     author: {
       name: "Samuyl Joshi",
